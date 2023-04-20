c0590c756369e5234f1e7414d6fd9100412cd532
son <tungson.ktct@gmail.com>
2023-04-20 10:00:07 +0700

    update-excel

--- tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-function run() {
-  var object1 = { foo: "bar", a: 5 };
-
-  var object2 = { name: "foo", d: 10 };
-
-  Object.assign(object1, object2);
-
-  console.log(object2);
-}
--- test.js
+++
@@ -1,6 +0,0 @@
-var o1 = { a: 1, b: 1, c: 1, d: 5 };
-var o2 = { b: 2, c: 2 };
-var o3 = { c: 3, d: 1 };
-
-var obj = Object.assign({}, o1, o2, o3);
-console.log(obj);
